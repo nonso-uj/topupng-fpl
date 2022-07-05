036fbf6291f6cec3b57071996c459c947d48b8b2
Udonne Chinonso <nonso.udonne@gmail.com>
2022-07-05 12:08:02 +0100

    Update prod.py

--- topupng_fpl/settings/prod.py
+++ topupng_fpl/settings/prod.py
@@ -22,51 +22,51 @@ ALLOWED_HOSTS = [
 
 
 
-# AWS S3 SETTINGS
+AWS S3 SETTINGS
 
-# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
-# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 
-# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
-# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-# AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = 'public-read'
 
-# AWS_S3_OBJECT_PARAMETERS = {
-#    'CacheControl': 'max-age=86400'
-# }
+AWS_S3_OBJECT_PARAMETERS = {
+   'CacheControl': 'max-age=86400'
+}
 
-# AWS_LOCATION = 'static'
+AWS_LOCATION = 'static'
 
-# AWS_QUERYSTRING_AUTH = False
+AWS_QUERYSTRING_AUTH = False
 
-# AWS_HEADERS = {
-#    'Access-Control-Allow-Origin': '*',
-# }
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+}
 
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
-# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 
-# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 
 
@@ -105,6 +105,6 @@ LOGGING = {
 
 
 
-django_on_heroku.settings(locals())
-# django_on_heroku.settings(locals(), staticfiles=False)
-# del DATABASES['default']['OPTIONS']['sslmode']
+# django_on_heroku.settings(locals())
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
