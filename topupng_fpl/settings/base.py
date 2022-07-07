1560e68d45001665bd11ee271892b599b266fa51
nonso-uj <nonso.udonne@gmail.com>
2022-07-07 23:45:42 +0100

    added passwords to environment variables

--- topupng_fpl/settings/base.py
+++ topupng_fpl/settings/base.py
@@ -145,7 +145,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.sendinblue.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nonso.udonne@gmail.com'
-EMAIL_HOST_PASSWORD = 'HqzRhLg6EcFADsS7'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 
