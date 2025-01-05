ee281bd24d011248963b54a816e28dc94bdcaca7
nonso_uj <nonso.udonne@gmail.com>
2025-01-05 02:50:17 +0100

    updated

--- orange_fpl/settings.py
+++ orange_fpl/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -133,6 +133,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
